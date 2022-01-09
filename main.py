e7a790be69742d7f2f7b364d59c27583a2a19542
ciomeczek <ciomek@tutanota.com>
2022-01-09 21:42:10 +0100

    Send deleted messages
    -Sends content of deleted messages

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import io
+import discord
+import aiohttp
 from bot import client
 from bot.actions import on_msg
 
@@ -11,6 +14,22 @@ async def on_ready():
 @client.event
 async def on_message(message):
     await on_msg(message)
+
+
+@client.event
+async def on_message_delete(message):
+    if message.author.bot:
+        return
+
+    files = []
+    async with aiohttp.ClientSession() as session:
+        for attachment in message.attachments:
+            async with session.get(attachment.url) as resp:
+                data = io.BytesIO(await resp.read())
+                files.append(discord.File(data, filename=attachment.url.split('/')[-1]))
+
+    await message.channel.send(f'<@{message.author.id}> deleted message: "{message.content}" at '
+                               f'{message.created_at.strftime("%m/%d/%Y, %H:%M:%S")}', files=files)
 
 
 client.run(os.getenv('BOT_TOKEN'))
